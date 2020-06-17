e171b47f25193e1064be97f2357d4b5385ae22ea
niap0r <zapf1@protonmail.com>
2020-06-17 12:33:01 +0200

    implemented custom host ip

--- app/src/main/java/com/example/iot_error_scanner_grp1/MainActivity.java
+++ app/src/main/java/com/example/iot_error_scanner_grp1/MainActivity.java
@@ -9,11 +9,30 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.text.Editable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.gms.vision.barcode.Barcode;
+
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.net.URL;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -21,8 +40,14 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnToScan;
     TextView TVresult;
+    EditText TVbackendIP;
 
-    // we don't need this number, can be any int > 0
+    public static final String URL_EXTENSION = "/api";
+    public static final String URL_FRONT = "http://";
+    // change port in production -> python flask uses 5000 by default for dev server
+    public static final String PORT = ":5000";
+
+
     public static final int REQUEST_CODE = 100;
     // 200 is usage permission code
     public static final int PERMISSION_REQUEST = 200;
@@ -36,6 +61,9 @@ public class MainActivity extends AppCompatActivity {
         btnToScan = findViewById(R.id.buttonToScan);
         TVresult  = findViewById(R.id.resultTextView);
 
+
+
+        // ask for camera permission
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
         }
@@ -51,17 +79,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    protected void onActivityResult(int requestCode, final int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
+        TVbackendIP = findViewById(R.id.backendIpTextView);
+        Editable editableURL = TVbackendIP.getText();
+        String baseURL = editableURL.toString();
 
         if (data != null)  {
             final Barcode barcode = data.getParcelableExtra("barcode");
-            TVresult.post(new Runnable() {
+
+            // HTTP request starts
+            String json = "{\"id\":1,\"error\":\"" + barcode.displayValue + "\"}";
+
+            Log.d("MYURL", URL_FRONT + baseURL + PORT + URL_EXTENSION);
+            OkHttpClient httpClient = new OkHttpClient();
+
+            RequestBody body = RequestBody.create(
+                    MediaType.parse("application/json"), json);
+
+            Request request = new Request.Builder()
+                    .url(URL_FRONT + baseURL + PORT + URL_EXTENSION)
+                    .post(body)
+                    .build();
+
+            httpClient.newCall(request).enqueue(new Callback() {
                 @Override
-                public void run() {
-                    TVresult.setText(barcode.displayValue);
+                public void onFailure(@NotNull Call call, @NotNull final IOException e) {
+
+                    Log.d("HTTP_CONN", e.toString());
+                }
+                @Override
+                public void onResponse(@NotNull Call call, @NotNull final Response response) throws IOException {
+
+                    String jsonData = response.body().string();
+                    try {
+                        JSONObject jsonObject = new JSONObject(jsonData);
+                        if (jsonObject.getBoolean("processed")) {
+                            TVresult.setText(jsonObject.getString("msg"));
+                        }
+                        Log.d("HTTP_CONN", jsonObject.getString("msg"));
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
                 }
             });
         }
     }
 }
+
--- app/src/main/java/com/example/iot_error_scanner_grp1/ScanActivity.java
+++ app/src/main/java/com/example/iot_error_scanner_grp1/ScanActivity.java
@@ -41,10 +41,8 @@ public class ScanActivity extends AppCompatActivity {
                 .setBarcodeFormats(Barcode.QR_CODE)
                 .build();
 
-
         cameraSource = new CameraSource.Builder(this, barcodeDetector)
                 .setFacing(CameraSource.CAMERA_FACING_BACK)
-                .setRequestedFps(24)
                 .setAutoFocusEnabled(true)
                 .setRequestedPreviewSize(1920, 1080)
                 .build();
